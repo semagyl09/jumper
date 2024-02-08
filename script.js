6b5fd8bb47beb1c29c1a10b56a4ddd9b247ba75e
Semen Gulyaev <semagylyev9@gmail.com>
2024-02-08 21:00:45 +0500

    alert to log

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ let isAlive = setInterval ( function() {
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
     if (cactusLeft < 50 && cactusLeft > 0 &&  dinoTop >= 140) {
-        alert("game OVER!!")
+        console.log("game OVER!!")
     }
 }, 10)
